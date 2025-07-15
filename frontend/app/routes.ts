9d765dbd9d74039a48c542db950c5399488e9347
astepien-godaddy <astepien@godaddy.com>
2025-07-14 23:15:46 -0700

    update-routes

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,3 +1,11 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/dashboard.tsx"),
+  route("crisis-alert-flow/page1", "routes/crisis-alert-flow/page1.jsx"),
+  route("crisis-alert-flow/page1b", "routes/crisis-alert-flow/page1b.jsx"),
+  route("crisis-alert-flow/page2", "routes/crisis-alert-flow/page2.jsx"),
+  route("crisis-alert-flow/page3", "routes/crisis-alert-flow/page3.jsx"),
+  route("crisis-alert-flow/page4", "routes/crisis-alert-flow/page4.jsx"),
+  route("crisis-alert-flow/page5", "routes/crisis-alert-flow/page5.jsx"),
+] satisfies RouteConfig;
